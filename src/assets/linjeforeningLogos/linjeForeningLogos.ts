7b3bcc0d6c7e704219924138216a1e4e6f19dd7a
FelixAnthonisen <felix.anthonisen@gmail.com>
2024-04-27 15:48:16 +0200

    new logo for tihlde

--- src/assets/linjeforeningLogos/linjeForeningLogos.ts
+++ src/assets/linjeforeningLogos/linjeForeningLogos.ts
@@ -1,7 +1,7 @@
 import Echo from "./echoTransparent.webp";
 import Abakus from "./abakusTransparent.png";
 import Online from "./onlineTransparent.png";
-import Thilde from "./thildeTransparent.jpeg";
+import Thilde from "./tihldeTransparent.webp";
 import Helix from "./helix.png";
 import Lffh from "./lffh.png"
 
