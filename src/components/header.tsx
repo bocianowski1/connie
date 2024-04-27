c779971b4ac59df1e95636ce768f3ce7523bdd28
Ole Magnus Fon Johnsen <me@omfj.no>
2024-04-27 11:23:57 +0200

    header noe

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,18 +1,39 @@
+import Link from "next/link";
+
+const items = [
+  {
+    name: "Finn nyutdannede",
+    href: "/finn-nyutdannede",
+  },
+  {
+    name: "Bli med",
+    href: "/",
+  },
+  {
+    name: "Logg inn",
+    href: "/logg-inn",
+  },
+];
+
 export const Header = () => {
   return (
-    <header className="flex items-center justify-between p-6">
+    <header className="flex items-center justify-between p-8 max-w-screen-2xl mx-auto">
       <div>
-        <a className="font-serif font-medium text-2xl" href="/">
+        <a className="font-serif font-medium text-3xl" href="/">
           Connie
         </a>
       </div>
 
       <menu>
-        <li>
-          <a className="text-sm font-medium" href="/logg-inn">
-            Logg inn
-          </a>
-        </li>
+        {items.map((item) => (
+          <Link
+            key={item.name}
+            href={item.href}
+            className="px-4 py-2 rounded-full hover:bg-gray-300 transition-all"
+          >
+            {item.name}
+          </Link>
+        ))}
       </menu>
     </header>
   );
