70f7b0d6615dcc1d2a3d51b0d0fa993c2625a821
Ole Magnus Fon Johnsen <me@omfj.no>
2024-04-27 16:11:43 +0200

    footer padding

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,7 +1,7 @@
 export const Footer = () => {
   return (
     <footer className="bg-primary py-12">
-      <div className="max-w-screen-lg mx-auto">
+      <div className="max-w-screen-lg mx-auto px-6">
         <div className="py-12 flex items-center justify-between">
           <div>
             <h2 className="text-2xl font-serif mb-4">Connie</h2>
