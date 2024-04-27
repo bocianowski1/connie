79ebc24298374bb078e332e99c88eb910e7dfd42
Ole Magnus Fon Johnsen <me@omfj.no>
2024-04-27 16:09:55 +0200

    Mvp header

--- src/components/logout.tsx
+++ src/components/logout.tsx
@@ -1,9 +1,12 @@
-"use client"
+"use client";
 import { logout } from "@/app/logg-inn/actions";
 
 export default function LogOutButton() {
   return (
-    <button onClick={() => logout()} className="text-sm font-medium">
+    <button
+      onClick={() => logout()}
+      className="bg-primary font-medium flex flex-row h-10 items-center justify-center px-4 text-white w-fit rounded-lg gap-4"
+    >
       Logg ut
     </button>
   );
