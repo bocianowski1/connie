68490789df58a5e6419565a26fca663a87261462
FelixAnthonisen <felix.anthonisen@gmail.com>
2024-04-27 16:06:20 +0200

    add more linjeforening logos

--- src/assets/linjeforeningLogos/linjeForeningLogos.ts
+++ src/assets/linjeforeningLogos/linjeForeningLogos.ts
@@ -4,5 +4,6 @@ import Online from "./onlineTransparent.png";
 import Thilde from "./tihldeTransparent.webp";
 import Helix from "./helix.png";
 import Lffh from "./lffh.png"
+import Mjolnir from "./mjolnirTransparent.png"
 
-export const linjeforeningLogos = [Echo, Abakus, Online, Thilde, Helix, Lffh];
+export const linjeforeningLogos = [Echo, Abakus, Online, Thilde, Helix, Lffh, Mjolnir];
