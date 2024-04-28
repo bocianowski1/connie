7b30fd7eff5ba6bd197683a09e84c83a416f587f
nikolaus <Nikolaus.Engh@student.uib.no>
2024-04-28 11:20:59 +0200

    added legend

--- src/app/bedrift/dashboard/page.tsx
+++ src/app/bedrift/dashboard/page.tsx
@@ -7,18 +7,24 @@ export default function DashBoard() {
   const feedback = Feedbacks[0];
   return (
     <div className="grid grid-cols-2">
-      <Link href="/bedrift/kontrakter" className="border m-2 p-4 rounded-lg hover:bg-lightgray">
+      <Link
+        href="/bedrift/kontrakter"
+        className="border m-2 p-4 rounded-lg hover:bg-lightgray"
+      >
         <p className="text-center text-2xl">Kontrakter</p>
       </Link>
-      <Link href="/bedrift/tilbakemeldinger" className="border m-2 p-4 rounded-lg hover:bg-lightgray">
+      <Link
+        href="/bedrift/tilbakemeldinger"
+        className="border m-2 p-4 rounded-lg hover:bg-lightgray"
+      >
         <p className="text-center text-2xl">Alle tilbakemeldinger</p>
       </Link>{" "}
       <div className="col-span-2 m-2 p-4 rounded-lg border">
-        <h3 className="text-2xl text-center font-bold mb-4">
+        <h3 className="text-2xl text-balance font-serif font-medium text-center">
           Tilbakemeldinger for siste arrangement
         </h3>
-        <p className="text-xl text-center">{feedback.eventTitle}</p>
-        <p className="text-center mb-6">{feedback.eventDate.toDateString()}</p>
+        <p className="text-lg text-center">{feedback.eventTitle}</p>
+        <p className="text-center mb-6 text-gray-500 text-sm">{feedback.eventDate.toDateString()}</p>
         <div className="flex">
           <FeedbackPreview {...feedback} />
         </div>
--- src/components/feedback-preview.tsx
+++ src/components/feedback-preview.tsx
@@ -11,14 +11,30 @@ export function FeedbackPreview({
     <div className="flex border p-6 rounded-lg">
       <div>
         <Image src={graph} alt="graph" height={300}></Image>
-        <p>
-            Vurdering:
-        </p>
+        <p>Vurdering:</p>
         <div className="flex justify-evenly">
           <div className="mr-8">1/4</div>
           <div className="mr-8">2/4</div>
           <div className="mr-8">3/4</div>
           <div className="mr-8">4/4</div>
+        </div>
+        <div className="flex flex-col m-2 gap-2">
+          <div className="flex gap-2">
+            <div className="rounded-full bg-teal-500 h-4 w-4"> </div>
+            <p>Datateknologi</p>
+          </div>
+          <div className="flex gap-2">
+            <div className="rounded-full bg-sky-300 h-4 w-4"></div>
+            <p>Data science</p>
+          </div>
+          <div className="flex gap-2">
+            <div className="rounded-full bg-fuchsia-500 h-4 w-4"></div>
+            <p>Datasikkerhet</p>
+          </div>
+          <div className="flex gap-2">
+            <div className="rounded-full bg-purple-700 h-4 w-4"></div>
+            <p>IMØ</p>
+          </div>
         </div>
       </div>
       <div className="flex flex-col gap-4">
