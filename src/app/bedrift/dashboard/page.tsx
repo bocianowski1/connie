a46bfe04825cb70e249a4872075b4b1e12c395a5
nikolaus <Nikolaus.Engh@student.uib.no>
2024-04-28 10:30:35 +0200

    added feedback

--- src/app/bedrift/dashboard/page.tsx
+++ src/app/bedrift/dashboard/page.tsx
@@ -1,14 +1,27 @@
+import { FeedbackPreview } from "@/components/feedback-preview";
+
+import { Feedbacks } from "./data";
+
 export default function DashBoard() {
+  const feedback = Feedbacks[0];
   return (
     <div className="grid grid-cols-2">
-      <div className="border rounded-lg">
-        <p>Kontraker</p>
+      <div className="border m-2 p-4 rounded-lg">
+        <p className="text-center text-2xl">Kontrakter</p>
       </div>
-      <div className="border rounded-lg">
-        <p>Profil</p>
+      <div className="border m-2 p-4 rounded-lg">
+        <p className="text-center text-2xl">Alle tilbakemeldinger</p>
+      </div>{" "}
+      <div className="col-span-2 m-2 p-4 rounded-lg border">
+        <h3 className="text-2xl text-center font-bold mb-4">
+          Tilbakemeldinger for siste arrangement
+        </h3>
+        <p className="text-xl text-center">{feedback.eventTitle}</p>
+        <p className="text-center mb-6">{feedback.eventDate.toDateString()}</p>
+        <div className="flex">
+          <FeedbackPreview {...feedback} />
+        </div>
       </div>
-
-      <div className="col-span-2">Tidligere arrangementer</div>
     </div>
   );
 }
